7aaa473db1115a91f390b1235609664b45108659
justice <xsh4k3@gmail.com>
2018-01-24 11:18:01 +0100

    Adding dependencies and some project structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Chatterbot
 Javascript implementation of the Chatterbot library
+
+//To Add
+- Babel
+- Webpack & co
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require("webpack")
+const path = require("path")
+const fs = require("fs")
+//Use nodemon to livereload our bot.
+const NodemonPlugin = require("nodemon-webpack-plugin")
+//Ignore node externals
+const nodeExternals = require("webpack-node-externals")
+
+module.exports = {
+  entry: "./chatterbot/chatterbot.js",
+  plugins: [
+    new NodemonPlugin(),
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  target: "node",
+  node: {
+    console: true
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  externals: nodeExternals(),
+  devtool: "sourcemap"
+}
